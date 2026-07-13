78ea6e4071bc1c54f9a839663fd9c04450181113
Lincoln <86622358+linquinho@users.noreply.github.com>
2026-07-13 12:44:45 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -160,7 +160,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["*ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
